8331a6385f8df09bf03ab972f49b73924de605e7
Ignacio Gomez <ignacio.gomez@axen.com>
2024-03-05 14:35:46 -0600

    Initial

---
+++ frontaxenacademy/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Index from './components/Index'
+
+function App() {
+
+  return (
+    <>
+      <Index></Index>
+    </>
+  )
+}
+
+export default App
---
+++ frontaxenacademy/src/components/Login.tsx
@@ -0,0 +1,20 @@
+import { Paper } from "@mui/material"
+
+const Index = () => {
+
+
+
+    return (
+        <>
+            <Paper
+                sx={{
+                    p: "24px",
+                    borderRadius: 8,
+                    marginBottom: "24px",
+                }}
+            />
+        </>
+    )
+}
+
+export default Index;
