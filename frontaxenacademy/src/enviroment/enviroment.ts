eafdd75d610cd0d1775dee6e3297ab70218da029
kilgore4 <robsanbad@gmail.com>
2024-03-05 19:12:00 -0600

    preparando enviorement

--- frontaxenacademy/src/enviroment/enviroment.ts
+++ frontaxenacademy/src/enviroment/enviroment.ts
@@ -2,4 +2,5 @@ export const Enviroment = {
     apiUsers: 'api/users',
 };
 //'https://bulkuploadstests.azurewebsites.net';//
-export const API_URL = process.env.REACT_APP_API_URL;
+console.log(import.meta.env.VITE_APP_API_URL);
+export const API_URL = import.meta.env.VITE_APP_API_URL;
