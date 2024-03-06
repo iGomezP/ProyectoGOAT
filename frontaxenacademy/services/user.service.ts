adb2ae2c68b4639a633d00294df34511b3a3c245
kilgore4 <robsanbad@gmail.com>
2024-03-05 21:54:37 -0600

    Cambios en servicios

---
+++ frontaxenacademy/services/user.service.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { loginModel } from "../models/loginModel";
+
+const login = (request: loginModel) => {
+    //axios.post
+};
--- frontaxenacademy/src/components/Login.tsx
+++ frontaxenacademy/src/components/Login.tsx
@@ -1,22 +1,49 @@
-import { Paper } from "@mui/material"
-
+import { Button } from "@mui/base";
+import { Paper, TextField, Typography } from "@mui/material"
+import  * as Yup from 'yup';
+import { useFormik } from 'formik';
+import {loginModel} from '../../models/loginModel';
 const Index = () => {
-
-
-
+    const initialValues: loginModel = {
+        "nickname": '',
+        "email": '',
+        "password": ''
+      };
+    const onSubmit = (data: loginModel) =>{
+        if(data){
+            // do something
+            
+        }
+    };
+    const { handleSubmit, handleChange, errors, values } = 
+    useFormik({
+        initialValues,
+        validationSchema: Yup.object({
+            
+        }),
+        onSubmit
+    });
     return (
         <>
             <Paper
                 sx={{
                     p: "24px",
                     borderRadius: 8,
-                    marginBottom: "24px",
+                    maxWidth:'24%'
                 }}
-            >
-                Hello
+            >   <Typography>Usuario:</Typography>
+                <TextField id="outlined-basic"  variant="outlined" />
+                <Typography>E-mail:</Typography>
+                <TextField id="outlined-basic"  variant="outlined" />
+                <Typography>Contrasena:</Typography>
+                <TextField id="outlined-basic"  variant="outlined" />
+                <Button>
+                    Iniciar sesion
+                </Button>
             </Paper>
+            
         </>
-    )
+    );
 }
 
 export default Index;
