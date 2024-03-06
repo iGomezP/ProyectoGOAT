463f45c82e7f8a12734e0fe706c057a6e2cf1ee7
kilgore4 <robsanbad@gmail.com>
2024-03-05 18:30:20 -0600

    Ajustando servicio para validar login

--- frontaxenacademy/src/enviroment/enviroment.ts
+++ frontaxenacademy/src/enviroment/enviroment.ts
@@ -1,4 +1,5 @@
 export const Enviroment = {
     apiUsers: 'api/users',
 };
-export const API_URL = 'https://bulkuploadstests.azurewebsites.net/';//process.env.REACT_APP_API_URL;
+//'https://bulkuploadstests.azurewebsites.net';//
+export const API_URL = process.env.REACT_APP_API_URL;
