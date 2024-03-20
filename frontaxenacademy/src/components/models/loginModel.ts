869bd1938b1a0498a2a43026653a02235142bb78
Ignacio Gomez <ignacio.gomez@axen.com>
2024-03-19 22:32:16 -0600

    avance css

---
+++ frontaxenacademy/src/components/models/loginModel.ts
@@ -0,0 +1,5 @@
+export interface loginModel {
+    "nickname": string,
+    "email": string,
+    "password": string
+  }
---
+++ frontaxenacademy/src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const Environment = {
+	apiUsers: '/api/users',
+};
+
+export const API_URL = import.meta.env.VITE_APP_API_URL;
